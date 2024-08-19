b6eb3f48982c6e64080c20ef691de08e2d95da6d
Mihai Valcu <mihai.c.valcu@gmail.com>
2024-08-19 16:06:08 +0300

    Initial commit

---
+++ src/test/java/org/awbd/libraryapp/BookspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.awbd.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
